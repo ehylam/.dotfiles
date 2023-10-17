612bac6346b370aaea342a0e106eee52db95a992
Eric Lam <ericlam@192-168-1-112.tpgi.com.au>
2023-10-17 22:24:37 +1100

    dot file updates

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -33,35 +33,35 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = {
     -- Typescript formatter
-    {
-      "davidosomething/format-ts-errors.nvim",
-      ft = "javascript,typescript,typescriptreact,svelte",
-    },
+    -- {
+    --   "davidosomething/format-ts-errors.nvim",
+    --   ft = "javascript,typescript,typescriptreact,svelte",
+    -- },
     -- Php
-    {
-      "gbprod/phpactor.nvim",
-      ft = { "php", "yaml" },
-      cmd = { "PhpActor" },
-      keys = {
-        { "<leader>pc", ":PhpActor context_menu<cr>", desc = "PhpActor context menu" },
-      },
-      build = function()
-        require("phpactor.handler.update")()
-      end,
-      opts = {
-        install = {
-          check_on_startup = "daily",
-          bin = vim.fn.stdpath("data") .. "/mason/bin/phpactor",
-        },
-        lspconfig = {
-          enabled = true,
-          init_options = {
-            ["language_server_phpstan.enabled"] = true,
-            ["phpunit.enabled"] = true,
-          },
-        },
-      },
-    },
+    -- {
+    --   "gbprod/phpactor.nvim",
+    --   ft = { "php", "yaml" },
+    --   cmd = { "PhpActor" },
+    --   keys = {
+    --     { "<leader>pc", ":PhpActor context_menu<cr>", desc = "PhpActor context menu" },
+    --   },
+    --   build = function()
+    --     require("phpactor.handler.update")()
+    --   end,
+    --   opts = {
+    --     install = {
+    --       check_on_startup = "daily",
+    --       bin = vim.fn.stdpath("data") .. "/mason/bin/phpactor",
+    --     },
+    --     lspconfig = {
+    --       enabled = true,
+    --       init_options = {
+    --         ["language_server_phpstan.enabled"] = true,
+    --         ["phpunit.enabled"] = true,
+    --       },
+    --     },
+    --   },
+    -- },
   },
   ---@class PluginLspOpts
   opts = {

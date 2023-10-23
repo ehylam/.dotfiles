0e73fa32350c63fcae678f21a53498d9ed1ff756
Eric Lam <eric@chriate.com.au>
2023-10-24 08:59:14 +1100

    dot file updates

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,11 +1,12 @@
-return {
-	{
-		"m4xshen/hardtime.nvim",
-		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-		opts = {
-			disable_mouse = false,
-			allow_different_key = true,
-			disabled_keys = {},
-		},
-	},
-}
+return {}
+-- return {
+-- 	{
+-- 		"m4xshen/hardtime.nvim",
+-- 		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+-- 		opts = {
+-- 			disable_mouse = false,
+-- 			allow_different_key = true,
+-- 			disabled_keys = {},
+-- 		},
+-- 	},
+-- }

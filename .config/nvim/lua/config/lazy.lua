d5b0b4f79041d2f5125fde30f8510acda847ad8f
Eric Lam <eric@chriate.com.au>
2023-11-24 10:46:05 +1100

    dot file changes

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lsp.none-ls" },
 		{ import = "lazyvim.plugins.extras.ui.alpha" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
+		{ import = "lazyvim.plugins.extras.coding.copilot" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
